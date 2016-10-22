45de8f0ba22246198e3bc93dd1c0e3aa3fd2455c
Muhammad Nabil Fauzan <nabilfm39@gmail.com>
2016-10-22 10:51:29 +0700

    update about page

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -18,13 +18,10 @@
 	</div>
 	<div class="section white">
 		<div class="container">
-			<p style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec semper erat lectus. Integer dui libero, pellentesque in semper sed, gravida non ante. Curabitur aliquet porta placerat. Nullam consectetur venenatis neque nec sagittis. In pretium dolor metus, at auctor arcu finibus et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec placerat felis. Vivamus porta laoreet tortor, vel tempor tortor mollis eget. Etiam ac purus tempor, viverra lectus vel, ultrices mi.
-
-				Donec vestibulum dolor ac ex pellentesque, quis laoreet neque dapibus. In hac habitasse platea dictumst. Vivamus porta ex lacus, et vulputate nulla cursus quis. Phasellus sed neque id turpis vestibulum finibus nec et justo. Vivamus sed nulla id est tempus elementum pellentesque ut velit. Etiam vel sapien quis felis fringilla tempus consequat in nibh. Praesent in odio eget massa malesuada vehicula. Fusce vitae ante nec diam hendrerit congue eget eu augue. Praesent mauris elit, consequat eget sem quis, viverra venenatis eros. Pellentesque mattis fringilla lectus in pellentesque.
-
-				Maecenas non libero gravida, porttitor lacus in, euismod lacus. Phasellus ornare dolor id commodo pulvinar. Curabitur consequat risus ac congue scelerisque. Duis ut pretium tortor, id porta urna. Morbi sit amet pharetra eros, at sollicitudin lacus. In tincidunt vestibulum urna id bibendum. Suspendisse gravida dolor vel facilisis pulvinar. Praesent eget magna a sapien mollis dignissim. Nullam sed urna at est euismod volutpat. Nam suscipit quam id felis rhoncus lobortis.
-
-				Proin ut maximus eros. Ut imperdiet eget metus sit amet pretium. Mauris pharetra erat rhoncus ipsum ornare aliquet. Nulla purus enim, sollicitudin non elit in, luctus aliquam diam. In ac malesuada tellus, vitae hendrerit urna. Vestibulum suscipit enim ac nisi tincidunt egestas. In ut nisi sit amet nunc facilisis commodo. Aliquam facilisis hendrerit luctus. Phasellus ultricies dictum risus, in pellentesque neque iaculis sit amet. Maecenas maximus orci a convallis dictum. Vivamus ultricies ex risus, quis condimentum dui consectetur ac. Sed rutrum velit at orci bibendum rhoncus. Nullam sed eros dolor. Quisque vehicula dictum blandit.</p>
+			<center><img src="http://nabilfm.esy.es/smbr/img/me.jpeg" alt="" style="width:10%"></center>
+            			<p style="text-align: justify">
+            			    Website Sistem Informasi Geografis dibangun untuk menyelesaikan Skripsi / Tugas Akhir di Universitas Gunadarma, Depok. Tujuannya yaitu agar membantu para orang tua untuk mengambil keputusan dalam memilih sekolah berdasarkan jarak. Selain itu, Website ini juga memberikan informasi mengenai Passing Grade SMP yang dari tahun 2015. Website ini dibangun menggunakan Laravel 5 PHP Framework, OpenStreetMap (Map Data), Leaflet JS (interaksi dengan peta), Leaflet routing-machine (petunjuk arah menuju lokasi Sekolah), MaterializeCSS Framework
+            			</p>
 		</div>
 	</div>
 @endsection
